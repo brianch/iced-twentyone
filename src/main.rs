517aa9f38dead9e263971538eb4b01456d755ed0
brianch <brianch@users.noreply.github.com>
2024-01-07 03:56:12 -0300

    13. Check for result when dealing player's cards

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use card::{Deck, Hand};
 
 #[derive(PartialEq)]
 enum GameStage {
-    Init, Dealing, Standing
+    Init, Dealing, Standing, PlayerWon, HouseWon, Tie
 }
 
 struct IcedTwentyOne {
@@ -65,6 +65,15 @@ impl Application for IcedTwentyOne {
             Message::DealCard => {
                 if let Some(new_card) = self.deck.deal_card() {
                     self.player_hand.push(new_card);
+                    if self.player_hand.value() == 21 {
+                        if self.dealer_hand.value() == 21 {
+                            self.game_stage = GameStage::Tie;
+                        } else {
+                            self.game_stage = GameStage::PlayerWon;
+                        }
+                    } else if self.player_hand.value() > 21 {
+                        self.game_stage = GameStage::HouseWon;
+                    }
                 }
             } Message::Start => {
                 self.game_stage = GameStage::Dealing;
@@ -78,11 +87,19 @@ impl Application for IcedTwentyOne {
     fn view(&self) -> Element<Message, iced::Renderer<theme::TwentyOneTheme>> {
         let mut dealer_hand_val = String::from("?");
         let mut dealer_row = Row::new().spacing(10);
+        let mut game_result = text("");
         if self.game_stage == GameStage::Init || self.game_stage == GameStage::Dealing {
             dealer_row = dealer_row
                 .push(image(String::from("img/") + &self.dealer_hand.cards[0].get_id() + ".png").height(Length::Fixed(200.)))
                 .push(image(String::from("img/back.png")).height(Length::Fixed(200.)));
         } else {
+            if self.game_stage == GameStage::PlayerWon {
+                game_result = text("You won!!! Congrats :)").size(50);
+            } else if self.game_stage == GameStage::HouseWon {
+                game_result = text("You Lost!").size(50);
+            } else if self.game_stage == GameStage::Tie {
+                game_result = text("It's a tie!!").size(50);
+            }
             for card in &self.dealer_hand.cards {
                 dealer_row = dealer_row.push(image(String::from("img/") + &card.get_id() + ".png").height(Length::Fixed(200.)));
             }
@@ -134,6 +151,7 @@ impl Application for IcedTwentyOne {
 
         let table_col = col![
             dealer_info,
+            game_result,
             player_info,
         ].align_items(iced::Alignment::Center).spacing(10);
 
