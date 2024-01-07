c30adb6335cc9b1381cebc73ab2badec3be2121e
brianch <brianch@users.noreply.github.com>
2024-01-06 22:49:23 -0300

    1.2 Hello World with the Application trait

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use iced::widget::{container, text};
-use iced::{Sandbox, Element, Settings};
+use iced::executor;
+use iced::{Application, Element, Settings, Theme, Command};
 
 #[derive(Default)]
 struct IcedTwentyOne;
@@ -8,22 +9,26 @@ struct IcedTwentyOne;
 enum Message {
 }
 
-impl Sandbox for IcedTwentyOne {
+impl Application for IcedTwentyOne {
+    type Executor = executor::Default;
+    type Flags = ();
     type Message = Message;
+    type Theme = Theme;
 
-    fn new() -> Self {
-        Self::default()
+    fn new(_flags: ()) -> (IcedTwentyOne, Command<Self::Message>) {
+        (IcedTwentyOne::default(), Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Iced Twenty-One")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
     }
 
-    fn view(&self) -> Element<Message> {
-        let hello = text("Hello Iced");
+    fn view(&self) -> Element<Self::Message> {
+        let hello = text("Hello, Iced! (with application trait)");
         container(hello).into()
     }
 }
