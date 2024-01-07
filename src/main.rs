5653012f0a495850292464ab67187d1117b8e01f
brianch <brianch@users.noreply.github.com>
2024-01-07 03:46:00 -0300

    10. Adding the dealer's hand

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,24 @@ use card::{Deck, Hand};
 struct IcedTwentyOne {
     deck: Deck,
     player_hand: Hand,
+    dealer_hand: Hand,
 }
 
 impl Default for IcedTwentyOne {
     fn default() -> IcedTwentyOne {
         let mut deck = Deck::new();
-        let mut hand = Hand::new();
+        let mut player = Hand::new();
+        let mut dealer = Hand::new();
         deck.shuffle();
-        hand.push(deck.deal_card().unwrap());
+        player.push(deck.deal_card().unwrap());
+        player.push(deck.deal_card().unwrap());
+        dealer.push(deck.deal_card().unwrap());
+        dealer.push(deck.deal_card().unwrap());
+
         IcedTwentyOne {
             deck: deck,
-            player_hand: hand,
+            player_hand: player,
+            dealer_hand: dealer
         }
     }
 }
@@ -56,16 +63,24 @@ impl Application for IcedTwentyOne {
     }
 
     fn view(&self) -> Element<Message, iced::Renderer<theme::TwentyOneTheme>> {
+        let mut dealer_row = Row::new();
+        for card in &self.dealer_hand.cards {
+            dealer_row = dealer_row.push(image(String::from("img/") + &card.get_id() + ".png").height(Length::Fixed(250.)));
+        }
+
         let mut player_row = Row::new();
         for card in &self.player_hand.cards {
             player_row = player_row.push(image(String::from("img/") + &card.get_id() + ".png").height(Length::Fixed(250.)));
         }
 
         let table_col = col![
+            text(self.dealer_hand.value().to_string()).size(35),
+            Rule::horizontal(4.),
+            dealer_row,
             player_row,
             Rule::horizontal(4.),
             text(self.player_hand.value().to_string()).size(35),
-            button(text("Deal another card")).on_press(Message::DealCard)
+            button(text("Deal another card")).on_press(Message::DealCard),
         ].align_items(iced::Alignment::Center).spacing(10);
 
         container(table_col)
